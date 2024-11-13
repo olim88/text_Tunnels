f4b5ae353de0febf9013b9d97c928522e431892b
olim <bobq4582@gmail.com>
2024-11-13 20:23:17 +0000

    Initial commit

---
+++ src/client/java/org/olim/text_tunnels/client/Text_tunnelsClient.java
@@ -0,0 +1,10 @@
+package org.olim.text_tunnels.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Text_tunnelsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/olim/text_tunnels/Text_tunnels.java
@@ -0,0 +1,10 @@
+package org.olim.text_tunnels;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Text_tunnels implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
