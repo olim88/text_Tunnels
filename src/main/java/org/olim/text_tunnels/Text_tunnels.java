108a26cf20d51f961b680b1f7a6b7230ec6d0e8c
olim <bobq4582@gmail.com>
2024-11-13 22:03:15 +0000

    had a working config open

--- src/main/java/org/olim/text_tunnels/Text_tunnels.java
+++ src/main/java/org/olim/text_tunnels/Text_tunnels.java
@@ -1,10 +1,12 @@
 package org.olim.text_tunnels;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.gui.screen.Screen;
 
 public class Text_tunnels implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        System.out.println("Text Tunnels Mod Initialized!");
     }
 }
