4f74c9795bd0e1c95c4c9143a0c7e4606a035c9e
olim88 <bobq4582@gmail.com>
2026-04-11 21:44:00 +0100

    fix crash again and remove redirect

--- src/main/java/org/olim/text_tunnels/mixins/ChatHudMixin.java
+++ src/main/java/org/olim/text_tunnels/mixins/ChatHudMixin.java
@@ -4,6 +4,8 @@ package org.olim.text_tunnels.mixins;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.ChatHudLine;
 import org.objectweb.asm.Opcodes;
 import org.olim.text_tunnels.MessageReceiveHandler;
 import org.spongepowered.asm.mixin.Final;
@@ -14,6 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mixin(ChatHud.class)
 
@@ -24,32 +27,32 @@ public class ChatHudMixin {
     @Final
     private List<ChatHudLine.Visible> visibleMessages;
 
-    @Redirect(method = "render(Lnet/minecraft/client/gui/hud/ChatHud$Backend;IIZ)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;visibleMessages:Ljava/util/List;", opcode = Opcodes.GETFIELD))
-    private List<ChatHudLine.Visible> textTunnels$beforeRender(ChatHud instance) {
-        return filterVisible();
+    @WrapOperation(method = "render(Lnet/minecraft/client/gui/hud/ChatHud$Backend;IIZ)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;visibleMessages:Ljava/util/List;", opcode = Opcodes.GETFIELD))
+    private List<ChatHudLine.Visible> textTunnels$beforeRender(ChatHud instance, Operation<List<ChatHudLine.Visible>> original) {
+        return filterVisible(instance, original);
     }
 
 
-    @Redirect(method = "scroll", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;visibleMessages:Ljava/util/List;", opcode = Opcodes.GETFIELD))
-    private List<ChatHudLine.Visible> textTunnels$editVisibleForScroll(ChatHud instance) {
-        return filterVisible();
+    @WrapOperation(method = "scroll", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;visibleMessages:Ljava/util/List;", opcode = Opcodes.GETFIELD))
+    private List<ChatHudLine.Visible> textTunnels$editVisibleForScroll(ChatHud instance, Operation<List<ChatHudLine.Visible>> original) {
+        return filterVisible(instance, original);
     }
 
-    @Redirect(method = "refresh", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;visibleMessages:Ljava/util/List;", opcode = Opcodes.GETFIELD))
-    private List<ChatHudLine.Visible> textTunnels$editRefresh(ChatHud instance) {
-        return filterVisible();
+    @WrapOperation(method = "refresh", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;visibleMessages:Ljava/util/List;", opcode = Opcodes.GETFIELD))
+    private List<ChatHudLine.Visible> textTunnels$editRefresh(ChatHud instance, Operation<List<ChatHudLine.Visible>> original) {
+        return filterVisible(instance, original);
     }
 
-    @Redirect(method = "forEachVisibleLine", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;visibleMessages:Ljava/util/List;", opcode = Opcodes.GETFIELD))
-    private List<ChatHudLine.Visible> textTunnels$editForEachVisible(ChatHud instance) {
-        return filterVisible();
+    @WrapOperation(method = "forEachVisibleLine", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/ChatHud;visibleMessages:Ljava/util/List;", opcode = Opcodes.GETFIELD))
+    private List<ChatHudLine.Visible> textTunnels$editForEachVisible(ChatHud instance, Operation<List<ChatHudLine.Visible>> original) {
+        return filterVisible(instance, original);
     }
 
     @Unique
-    private List<ChatHudLine.Visible> filterVisible() {
+    private List<ChatHudLine.Visible> filterVisible(ChatHud instance, Operation<List<ChatHudLine.Visible>> original) {
         if (MessageReceiveHandler.isFilterInActive()) {
-            return visibleMessages;
+            original.call(instance);
         }
-        return visibleMessages.stream().filter(message -> MessageReceiveHandler.shouldShow(message.addedTime())).toList();
+        return visibleMessages.stream().filter(message -> MessageReceiveHandler.shouldShow(message.addedTime())).collect(Collectors.toCollection(ArrayList::new));
     }
 }
